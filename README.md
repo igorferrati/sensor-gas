a7b0a1d22e636d6303e358f5b013110d3eb4a6e9
igorferrati <ferrati.igor@gmail.com>
2024-05-08 19:17:57 -0300

    Atualizacoes

--- README.md
+++ README.md
@@ -52,8 +52,64 @@ Esquema:
 
 ## Dashboards
 
-Disponível em:
-Disponível em:
-
 <img src="./imagens/metrics.png" alt="Métricas" width="400" height="200">
 <img src="./imagens/nivel-gas.png" alt="Nível" width="200" height="180">
+
+# Execução
+
+1. Faça as ligações pela protoboard dos dispositivos.
+
+2. Conecte a uma máquina.
+
+3. Será necessário ```python``` e ```C++``` e uma IDE.
+
+4. Execute o código de controle do arduino:
+
+``` C++
+#define sensorAnalogico A0
+#define ledverde 4
+#define ledvermelho 3
+
+void setup() {
+  pinMode(sensorDigital, INPUT);
+  pinMode(ledverde, OUTPUT);
+  pinMode(ledvermelho, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(ledverde, LOW);
+  digitalWrite(ledvermelho, LOW);
+}
+
+void loop() {
+  int ValorA = analogRead(sensorAnalogico);
+
+  if (ValorA <= 40) {
+    Serial.print("Sensor Analogico: ");
+    Serial.println(ValorA);
+    Serial.println("Qualidade do ar normal");
+    digitalWrite(ledverde, HIGH);
+    digitalWrite(ledvermelho, LOW);
+  } 
+  else if (ValorA > 40 && ValorA <= 75) {
+    Serial.print("Sensor Analogico: ");
+    Serial.println(ValorA);
+    Serial.println("ATENCAO: Presença de gases nocivos no ar");
+    digitalWrite(ledverde, LOW);
+    digitalWrite(ledvermelho, LOW);
+  } 
+  else {
+    Serial.print("Sensor Analogico: ");
+    Serial.println(ValorA);
+    Serial.println("CUIDADO: Qualidade do ar comprometida");
+    digitalWrite(ledvermelho, HIGH);
+    digitalWrite(ledverde, LOW);
+  }
+
+  delay(2000);
+}
+```
+
+5. Publicando dados via MQTT:
+
+ * Localmente, gerencie um broker de sua escolha e utiliza o script: ```local-broker.py```
+
+ * Para conectar com brokers externos ajuste o usuário e senha e endpoint do broker, exemplo ```adafruit.py```
--- adafruit.py
+++ adafruit.py
@@ -1,49 +1,40 @@
 import serial
 import paho.mqtt.client as mqtt
-import ssl
+import time
 
-# Configurações da porta serial
 port = "COM5"  # tools > port
 baudrate = 9600
 
 mqtt_broker = 'io.adafruit.com'
 mqtt_port = 1883
 mqtt_username = 'igorferrati'
-mqtt_password = '' 
+mqtt_password = 'PASSWORD' 
 mqtt_topics = ['igorferrati/feeds/data-output', 'igorferrati/feeds/gas', 'igorferrati/feeds/metrics']
 
-def on_connect(client, userdata, flags, rc):
-    print("Conectado ao broker MQTT com código de resultado: "+str(rc))
-
-def on_publish(client, userdata, mid):
-    print("Mensagem publicada com sucesso")
-
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_publish = on_publish
 
-# Conecta ao broker MQTT com SSL
 client.username_pw_set(mqtt_username, mqtt_password)
-# client.tls_set_context(context=ssl.create_default_context())
 client.connect(mqtt_broker, mqtt_port, 60)
 
 # Abre a porta serial
 serial_port = serial.Serial(port, baudrate)
 
-# Loop principal
 while True:
     # Lê uma linha da porta serial
-    data = serial_port.readline().decode().strip()
+    line1 = serial_port.readline().decode().strip()
+    line2 = serial_port.readline().decode().strip()
+    data = line1 + "\n" + line2
     print("Dados lidos:", data)
 
     # Publica os dados no tópico MQTT apropriado
-    if data.startswith("Sensor Analogico:"):
-        gas_value = data.split(":")[-1].strip()
-        client.publish(mqtt_topics[0], data)
+    if line1.startswith("Sensor Analogico:"):
+        gas_value = line1.split(":")[-1].strip()
+        # client.publish(mqtt_topics[0], data)
         client.publish(mqtt_topics[1], gas_value)
-        client.publish(mqtt_topics[2], gas_value)
-    else:
-        print("Formato de dados não reconhecido.")
+        # client.publish(mqtt_topics[2], gas_value)
+        
+        #limit 30 publish por min
+        time.sleep(2)
 
 # Fecha a porta serial
 serial_port.close()
